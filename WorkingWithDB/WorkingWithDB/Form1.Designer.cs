ba9a498ea196331ac4eccc3db29e14f86611fed1
Vladimir <mysuckriface1@gmail.com>
2018-12-04 10:54:54 +0300

    upload

---
+++ WorkingWithDB/WorkingWithDB/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WorkingWithDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bt_load = new System.Windows.Forms.Button();
+            this.bt_export = new System.Windows.Forms.Button();
+            this.bt_update = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 146);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1274, 417);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // bt_load
+            // 
+            this.bt_load.Location = new System.Drawing.Point(25, 57);
+            this.bt_load.Name = "bt_load";
+            this.bt_load.Size = new System.Drawing.Size(75, 23);
+            this.bt_load.TabIndex = 1;
+            this.bt_load.Text = "Load";
+            this.bt_load.UseVisualStyleBackColor = true;
+            this.bt_load.Click += new System.EventHandler(this.bt_load_Click);
+            // 
+            // bt_export
+            // 
+            this.bt_export.Location = new System.Drawing.Point(267, 57);
+            this.bt_export.Name = "bt_export";
+            this.bt_export.Size = new System.Drawing.Size(75, 23);
+            this.bt_export.TabIndex = 3;
+            this.bt_export.Text = "Export";
+            this.bt_export.UseVisualStyleBackColor = true;
+            this.bt_export.Click += new System.EventHandler(this.bt_export_Click);
+            // 
+            // bt_update
+            // 
+            this.bt_update.Location = new System.Drawing.Point(140, 57);
+            this.bt_update.Name = "bt_update";
+            this.bt_update.Size = new System.Drawing.Size(75, 23);
+            this.bt_update.TabIndex = 4;
+            this.bt_update.Text = "Update";
+            this.bt_update.UseVisualStyleBackColor = true;
+            this.bt_update.Click += new System.EventHandler(this.bt_add_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "10",
+            "20",
+            "30",
+            "40",
+            ">50"});
+            this.comboBox1.Location = new System.Drawing.Point(389, 59);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(389, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Фильтр по ID_PERSON";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1298, 575);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.bt_update);
+            this.Controls.Add(this.bt_export);
+            this.Controls.Add(this.bt_load);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button bt_load;
+        private System.Windows.Forms.Button bt_export;
+        private System.Windows.Forms.Button bt_update;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WorkingWithDB/WorkingWithDB/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+
+
+
+namespace WorkingWithDB
+{
+    public partial class Form1 : Form
+    {
+       
+        List<string> collect = new List<string>();
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            collect.Add("Открытие программы: "+DateTime.Now.ToString());
+
+
+
+        }
+        OleDbConnection con = new OleDbConnection("Provider = MSDAORA;DATA SOURCE=localhost:1521/xe;" +
+            "PASSWORD=palace5;PERSIST SECURITY INFO=True;USER ID=system");
+
+
+
+
+        private void bt_load_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbDataAdapter adapter = new OleDbDataAdapter("SELECT ID_PERSON ,PERSON.NAME,DATE_BIRTH AS BIRTH, MONEY," +
+                " CAR.NAME AS CAR_NAME,CAR.DATE_MANUFACTURE AS CAR_DATE,CAR.COST AS CAR_COST, CAR.RATING AS CAR_RATING," +
+                " PHONE.NAME AS PHONE_NAME, PHONE.DATE_MANUFACTURE AS PHONE_DATE, PHONE.COST AS PHONE_COST," +
+                " PHONE.RATING AS PHONE_RATING FROM PERSON JOIN CAR ON ID_PERSON = CAR.ID_CAR " +
+                "JOIN PHONE ON ID_PERSON = PHONE.ID_PHONE ", con);
+            DataTable datatable = new DataTable();
+
+            adapter.Fill(datatable);
+            dataGridView1.DataSource = datatable;
+            con.Close();
+
+        }
+
+        private void bt_export_Click(object sender, EventArgs e)
+        {
+            collect.Add("Нажат export: " + DateTime.Now.ToString());
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel Document (*.xls)|*.xls";
+            sfd.FileName = "export.xls";
+            if (sfd.ShowDialog()== DialogResult.OK)
+            {
+                ToExcel(dataGridView1, sfd.FileName);
+            }
+
+        }
+
+        private void ToExcel(DataGridView dGV, string filename)
+        {
+
+            string stOutput = "";
+            string sHeaders = "";
+            for (int j = 0; j < dGV.Columns.Count; j++)
+                   sHeaders = sHeaders.ToString() + Convert.ToString(dGV.Columns[j].HeaderText) + "\t";
+                stOutput += sHeaders + "\r\n";
+                for (int i = 0; i < dGV.RowCount - 1; i++)
+                {
+                    string stline = "";
+                    for (int j = 0; j < dGV.Rows[i].Cells.Count; j++)
+                        stline = stline.ToString() + Convert.ToString(dGV.Rows[i].Cells[j].Value) + "\t";
+                    stOutput += stline + "\r\n";
+                }
+                Encoding utf16 = Encoding.GetEncoding(1254);
+                byte[] output = utf16.GetBytes(stOutput);
+                FileStream fs = new FileStream(filename, FileMode.Create);
+                BinaryWriter bw = new BinaryWriter(fs);
+                bw.Write(output, 0, output.Length);
+                bw.Flush();
+                bw.Close();
+                fs.Close();
+        }
+
+       
+
+        private void bt_add_Click(object sender, EventArgs e)
+        {
+            collect.Add("Нажата Update: "  +DateTime.Now.ToString());
+            con.Open();
+            OleDbCommand cmd = new OleDbCommand("DEL", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.ExecuteReader();
+
+            OleDbCommand cmd1 = new OleDbCommand("ADD_D", con);
+            cmd1.CommandType = CommandType.StoredProcedure;
+            cmd1.ExecuteReader();
+            con.Close();
+        }
+
+        
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    collect.Add("Выбран фильтр 10: " + DateTime.Now.ToString());
+                    con.Open();
+                    OleDbDataAdapter adapter1 = new OleDbDataAdapter("SELECT ID_PERSON ,PERSON.NAME,DATE_BIRTH AS BIRTH, MONEY," +
+                " CAR.NAME AS CAR_NAME,CAR.DATE_MANUFACTURE AS CAR_DATE,CAR.COST AS CAR_COST, CAR.RATING AS CAR_RATING," +
+                " PHONE.NAME AS PHONE_NAME, PHONE.DATE_MANUFACTURE AS PHONE_DATE, PHONE.COST AS PHONE_COST," +
+                " PHONE.RATING AS PHONE_RATING FROM PERSON JOIN CAR ON ID_PERSON = CAR.ID_CAR " +
+                "JOIN PHONE ON ID_PERSON = PHONE.ID_PHONE  WHERE ID_PERSON <=10", con);
+                    DataTable datatable1 = new DataTable();
+
+                    adapter1.Fill(datatable1);
+                    dataGridView1.DataSource = datatable1;
+                    con.Close();
+
+                    break;
+
+                case 1:
+                    collect.Add("Выбран фильтр 20: " + DateTime.Now.ToString());
+                    con.Open();
+                    OleDbDataAdapter adapter2 = new OleDbDataAdapter("SELECT ID_PERSON ,PERSON.NAME,DATE_BIRTH AS BIRTH, MONEY," +
+                " CAR.NAME AS CAR_NAME,CAR.DATE_MANUFACTURE AS CAR_DATE,CAR.COST AS CAR_COST, CAR.RATING AS CAR_RATING," +
+                " PHONE.NAME AS PHONE_NAME, PHONE.DATE_MANUFACTURE AS PHONE_DATE, PHONE.COST AS PHONE_COST," +
+                " PHONE.RATING AS PHONE_RATING FROM PERSON JOIN CAR ON ID_PERSON = CAR.ID_CAR " +
+                "JOIN PHONE ON ID_PERSON = PHONE.ID_PHONE  WHERE ID_PERSON <=20", con);
+                    DataTable datatable2 = new DataTable();
+
+                    adapter2.Fill(datatable2);
+                    dataGridView1.DataSource = datatable2;
+                    con.Close();
+
+                    break;
+
+                case 2:
+                    collect.Add("Выбран фильтр 30: " + DateTime.Now.ToString());
+                    con.Open();
+                    OleDbDataAdapter adapter3 = new OleDbDataAdapter("SELECT ID_PERSON ,PERSON.NAME,DATE_BIRTH AS BIRTH, MONEY," +
+                " CAR.NAME AS CAR_NAME,CAR.DATE_MANUFACTURE AS CAR_DATE,CAR.COST AS CAR_COST, CAR.RATING AS CAR_RATING," +
+                " PHONE.NAME AS PHONE_NAME, PHONE.DATE_MANUFACTURE AS PHONE_DATE, PHONE.COST AS PHONE_COST," +
+                " PHONE.RATING AS PHONE_RATING FROM PERSON JOIN CAR ON ID_PERSON = CAR.ID_CAR " +
+                "JOIN PHONE ON ID_PERSON = PHONE.ID_PHONE  WHERE ID_PERSON <=30", con);
+                    DataTable datatable3 = new DataTable();
+
+                    adapter3.Fill(datatable3);
+                    dataGridView1.DataSource = datatable3;
+                    con.Close();
+
+                    break;
+
+
+                case 3:
+                    collect.Add("Выбран фильтр 40: " + DateTime.Now.ToString());
+                    con.Open();
+                    OleDbDataAdapter adapter4 = new OleDbDataAdapter("SELECT ID_PERSON ,PERSON.NAME,DATE_BIRTH AS BIRTH, MONEY," +
+                " CAR.NAME AS CAR_NAME,CAR.DATE_MANUFACTURE AS CAR_DATE,CAR.COST AS CAR_COST, CAR.RATING AS CAR_RATING," +
+                " PHONE.NAME AS PHONE_NAME, PHONE.DATE_MANUFACTURE AS PHONE_DATE, PHONE.COST AS PHONE_COST," +
+                " PHONE.RATING AS PHONE_RATING FROM PERSON JOIN CAR ON ID_PERSON = CAR.ID_CAR " +
+                "JOIN PHONE ON ID_PERSON = PHONE.ID_PHONE  WHERE ID_PERSON <=40", con);
+                    DataTable datatable4 = new DataTable();
+
+                    adapter4.Fill(datatable4);
+                    dataGridView1.DataSource = datatable4;
+                    con.Close();
+
+                    break;
+
+
+                case 4:
+                    collect.Add("Выбран фильтр 50+: " + DateTime.Now.ToString());
+                    con.Open();
+                    OleDbDataAdapter adapter5 = new OleDbDataAdapter("SELECT ID_PERSON ,PERSON.NAME,DATE_BIRTH AS BIRTH, MONEY," +
+                " CAR.NAME AS CAR_NAME,CAR.DATE_MANUFACTURE AS CAR_DATE,CAR.COST AS CAR_COST, CAR.RATING AS CAR_RATING," +
+                " PHONE.NAME AS PHONE_NAME, PHONE.DATE_MANUFACTURE AS PHONE_DATE, PHONE.COST AS PHONE_COST," +
+                " PHONE.RATING AS PHONE_RATING FROM PERSON JOIN CAR ON ID_PERSON = CAR.ID_CAR " +
+                "JOIN PHONE ON ID_PERSON = PHONE.ID_PHONE  WHERE ID_PERSON >=50", con);
+                    DataTable datatable5 = new DataTable();
+
+                    adapter5.Fill(datatable5);
+                    dataGridView1.DataSource = datatable5;
+                    con.Close();
+
+                    break;
+
+            }
+
+        }
+
+  
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            collect.Add("Закрытие программы: "+DateTime.Now.ToString());
+
+            string path = "log.txt";
+
+            if (!File.Exists(path))
+            {            
+                File.WriteAllLines(path, collect.ToArray(), Encoding.UTF8);
+            }
+            else
+            {
+                File.AppendAllLines(path, collect, Encoding.UTF8);
+            }
+        }
+    }
+}
